eb805e598017a273ee5be5232d10f9ee2fc75a87
Sadriddin <musayevsd@mail.ru>
2024-11-08 16:33:12 +0500

    .

--- routes/main/main.js
+++ routes/main/main.js
@@ -3,22 +3,29 @@ const mainRoutes = Router()
 
 mainRoutes.get('/en', (req, res) => {
     res.render('home_en', {
-        hasNotify: true,
+        hasNotify: false,
         title: "Dear customers!",
         message: "Due to popular demand, we are accepting orders until 10 PM.",
-        lang: "en",
-        host: process.env.HOST
+        lang: "en"
     });
 });
 
 mainRoutes.get('/uz', (req, res) => {
     res.render('home_uz', {
-        hasNotify: true,
+        hasNotify: false,
         title: "Hurmatli xaridorlar!",
         message: "Ko‘p sonli iltimoslarga ko‘ra bugun buyurtmalarni soat 22:00 gacha qabul qilamiz",
-        lang: "uz",
-        host: process.env.HOST
+        lang: "uz"
     });
+});
+
+mainRoutes.get('/ru', (req, res) => {
+        res.render('home_ru', {
+            hasNotify: false,
+            title: "Hurmatli xaridorlar!",
+            message: "Ko‘p sonli iltimoslarga ko‘ra bugun buyurtmalarni soat 22:00 gacha qabul qilamiz",
+            lang: "ru"
+        });
 });
 
 module.exports = mainRoutes
